cfe826c62907977e01a0a3da83b6c8720b50fed4
Sadra <lnxpylnxpy@gmail.com>
2022-07-14 17:27:21 +0430

    new theme ovserved

---
+++ snippet/static/js/bs-init.js
@@ -0,0 +1,12 @@
+
+if (window.innerWidth < 768) {
+	[].slice.call(document.querySelectorAll('[data-bss-disabled-mobile]')).forEach(function (elem) {
+		elem.classList.remove('animated');
+		elem.removeAttribute('data-bss-hover-animate');
+		elem.removeAttribute('data-aos');
+	});
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+	AOS.init();
+}, false);

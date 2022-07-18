8ce6a468f1e51b7a72d12066b932bc6e28340d87
Sadra <lnxpylnxpy@gmail.com>
2022-07-18 18:08:39 +0430

    views and models tests completed

---
+++ snippet/tests/test_api_views.py
@@ -0,0 +1,51 @@
+from pdb import post_mortem
+from urllib import response
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from snippet.models import Snippet
+from snippet.constants import LANGUAGES
+import json
+from snippet.serializers import SnippetSerializer
+
+
+class SnippetAPITestCase(APITestCase):
+    
+    def setUp(self):
+        self.sample = {
+            'title': 'sample title',
+            'body': 'code snippet',
+            'language': LANGUAGES[0][0],
+        }
+        
+    def test_create_snippet(self):
+        url = reverse('create-paste')
+        response = self.client.post(
+            url,
+            json.dumps(self.sample),
+            content_type='application/json'
+        )
+        
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Snippet.objects.count(), 1)
+        self.assertEqual(Snippet.objects.get().title, 'sample title')
+    
+    def test_if_object_exists(self):
+        post_response = self.client.post(
+            reverse('create-paste'),
+            json.dumps(self.sample),
+            content_type='application/json'
+        )
+        
+        id = post_response.json()['id']
+
+        get_response = self.client.get(
+            reverse('get-paste', args=[id]),
+            content_type='application/json'
+        )
+        
+        get_serialized = SnippetSerializer(get_response.json()).data
+        
+        self.assertEqual(post_response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(get_response.status_code, status.HTTP_200_OK)
+        self.assertEqual(get_serialized, post_response.json())

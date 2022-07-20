f6f5ae4a4ea514f34c5fbe7f1654d52efe810f32
Sadra <lnxpylnxpy@gmail.com>
2022-07-20 15:17:14 +0430

    theme field added to serializer

--- snippet/serializers.py
+++ snippet/serializers.py
@@ -1,7 +1,6 @@
 from os import path
 
 from django.conf import settings
-from pyexpat import model
 from rest_framework.serializers import ModelSerializer
 
 from .models import Snippet
@@ -12,9 +11,10 @@ class SnippetSerializer(ModelSerializer):
     def to_representation(self, instance):
         data = super().to_representation(instance)
         data['url'] = path.join(settings.DOMAIN_URL, 'paste', data['id'])
+        data['theme'] = instance.get_theme_display()
         return data
 
     class Meta:
         model = Snippet
-        fields = ['id', 'title', 'body', 'language']
+        fields = ['id', 'title', 'body', 'language', 'theme']
         read_only_fields = ['id']

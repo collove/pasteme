5d2eede0d0159047d940849fd2cc5c22f35d49e5
Sadra <lnxpylnxpy@gmail.com>
2022-07-29 14:09:15 +0430

    database settings updated

--- pasteme/settings.py
+++ pasteme/settings.py
@@ -82,14 +82,15 @@ WSGI_APPLICATION = 'pasteme.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        # 'OPTIONS': {
-        #     'ssl': {'ca': config('MYSQL_ATTR_SSL_CA')},
-        #     'charset': 'utf8mb4',
-        #  }
-    }
+  'default': {
+    'ENGINE': 'django_psdb_engine',
+    'NAME': config('DB_DATABASE'),
+    'HOST': config('DB_HOST'),
+    'PORT': config('DB_PORT'),
+    'USER': config('DB_USER'),
+    'PASSWORD': config('DB_PASSWORD'),
+    'OPTIONS': {'ssl': {'ca': config('MYSQL_ATTR_SSL_CA')}, 'charset': 'utf8mb4'}
+  }
 }
 
 # Password validation

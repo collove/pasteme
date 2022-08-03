28b85df9ee7ceff3a08c9829d9b6d66e6e17bfde
Farhad <farhaduneci@gmail.com>
2022-08-03 16:58:28 +0430

    ADD: Hero Section

--- tailwindcss/static_src/tailwind.config.js
+++ tailwindcss/static_src/tailwind.config.js
@@ -48,6 +48,16 @@ module.exports = {
     fontFamily: {
       sans: ["Manrope", ...defaultTheme.fontFamily.sans],
     },
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+      },
+    },
     extend: {
       backgroundImage: {
         "body-pattern": "url('/static/img/pattern.svg')",

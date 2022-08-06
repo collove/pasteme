50b887dee5a0f76b008df4f1376729a8e846b694
Farhad <farhaduneci@gmail.com>
2022-08-06 15:35:23 +0430

    ADD: Blog styles

--- tailwindcss/static_src/tailwind.config.js
+++ tailwindcss/static_src/tailwind.config.js
@@ -8,81 +8,85 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-  content: [
-    /**
-     * HTML. Paths to Django template files that will contain Tailwind CSS classes.
-     */
+    content: [
+        /**
+         * HTML. Paths to Django template files that will contain Tailwind CSS classes.
+         */
 
-    /*  Templates within theme app (<tailwind_app_name>/templates), e.g. base/_base.html. */
-    "../templates/**/*.html",
+        /*  Templates within theme app (<tailwind_app_name>/templates), e.g. base/_base.html. */
+        "../templates/**/*.html",
 
-    /*
-     * Main templates directory of the project (BASE_DIR/templates).
-     * Adjust the following line to match your project structure.
-     */
-    "../../templates/**/*.html",
+        /*
+         * Main templates directory of the project (BASE_DIR/templates).
+         * Adjust the following line to match your project structure.
+         */
+        "../../templates/**/*.html",
 
-    /*
-     * Templates in other django apps (BASE_DIR/<any_app_name>/templates).
-     * Adjust the following line to match your project structure.
-     */
-    "../../**/templates/**/*.html",
+        /*
+         * Templates in other django apps (BASE_DIR/<any_app_name>/templates).
+         * Adjust the following line to match your project structure.
+         */
+        "../../**/templates/**/*.html",
 
-    /**
-     * JS: If you use Tailwind CSS in JavaScript, uncomment the following lines and make sure
-     * patterns match your project structure.
-     */
-    /* JS 1: Ignore any JavaScript in node_modules folder. */
-    // '!../../**/node_modules',
-    /* JS 2: Process all JavaScript files in the project. */
-    // '../../**/*.js',
+        /**
+         * JS: If you use Tailwind CSS in JavaScript, uncomment the following lines and make sure
+         * patterns match your project structure.
+         */
+        /* JS 1: Ignore any JavaScript in node_modules folder. */
+        // '!../../**/node_modules',
+        /* JS 2: Process all JavaScript files in the project. */
+        // '../../**/*.js',
 
-    /**
-     * Python: If you use Tailwind CSS classes in Python, uncomment the following line
-     * and make sure the pattern below matches your project structure.
-     */
-    // '../../**/*.py'
-  ],
-  darkMode: "class",
-  theme: {
-    fontFamily: {
-      sans: ["Manrope", ...defaultTheme.fontFamily.sans],
+        /**
+         * Python: If you use Tailwind CSS classes in Python, uncomment the following line
+         * and make sure the pattern below matches your project structure.
+         */
+        // '../../**/*.py'
+    ],
+    darkMode: "class",
+    theme: {
+        fontFamily: {
+            sans: ["Manrope", ...defaultTheme.fontFamily.sans],
+        },
+        container: {
+            center: true,
+            padding: "2rem",
+            screens: {
+                sm: "640px",
+                md: "768px",
+                lg: "1024px",
+                xl: "1280px",
+            },
+        },
+        extend: {
+            backgroundImage: {
+                "icon-light": "url('/static/img/icon-dark.svg')",
+                "icon-dark": "url('/static/img/icon-light.svg')",
+                "github-light": "url('/static/img/Github-dark.png')",
+                "github-dark": "url('/static/img/Github-light.png')",
+                "collove-light": "url('/static/img/collove-light.svg')",
+                "collove-dark": "url('/static/img/collove-dark.svg')",
+            },
+            flexGrow: {
+                2: 2,
+                3: 3,
+            },
+        },
     },
-    container: {
-      center: true,
-      padding: "2rem",
-      screens: {
-        sm: "640px",
-        md: "768px",
-        lg: "1024px",
-        xl: "1280px",
-      },
+    variants: {
+        extend: {
+            backgroundImage: ["dark"],
+        },
     },
-    extend: {
-      backgroundImage: {
-        "icon-light": "url('/static/img/icon-dark.svg')",
-        "icon-dark": "url('/static/img/icon-light.svg')",
-        "github-light": "url('/static/img/Github-dark.png')",
-        "github-dark": "url('/static/img/Github-light.png')",
-        "collove-light": "url('/static/img/collove-light.svg')",
-        "collove-dark": "url('/static/img/collove-dark.svg')",
-      },
-    },
-  },
-  variants: {
-    extend: {
-      backgroundImage: ["dark"],
-    },
-  },
-  plugins: [
-    /**
-     * '@tailwindcss/forms' is the forms plugin that provides a minimal styling
-     * for forms. If you don't like it or have own styling for forms,
-     * comment the line below to disable '@tailwindcss/forms'.
-     */
-    // require("@tailwindcss/forms"),
-    require("@tailwindcss/typography"),
-    // require('@tailwindcss/line-clamp'),
-    // require('@tailwindcss/aspect-ratio'),
-  ],
+    plugins: [
+        /**
+         * '@tailwindcss/forms' is the forms plugin that provides a minimal styling
+         * for forms. If you don't like it or have own styling for forms,
+         * comment the line below to disable '@tailwindcss/forms'.
+         */
+        // require("@tailwindcss/forms"),
+        require("@tailwindcss/typography"),
+        // require('@tailwindcss/line-clamp'),
+        // require('@tailwindcss/aspect-ratio'),
+    ],
 };

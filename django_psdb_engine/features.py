57c4f527a4f93778d8bfef96f2561b62ceae3d29
Sadra <lnxpylnxpy@gmail.com>
2022-08-15 20:43:22 +0430

    module removed

--- django_psdb_engine/features.py
+++
@@ -1,7 +0,0 @@
-from django.db.backends.mysql.features import (
-    DatabaseFeatures as MysqlBaseDatabaseFeatures,
-)
-
-
-class DatabaseFeatures(MysqlBaseDatabaseFeatures):
-    supports_foreign_keys = False

753582884d4cb18333f40649f8279bf508c2bb03
Farhad <farhaduneci@gmail.com>
2022-08-21 20:54:09 +0430

    UPDATE: General Style Updates

--- snippet/static/js/app.js
+++ snippet/static/js/app.js
@@ -14,3 +14,41 @@ $(function () {
         $("#menu").toggleClass("menu-open");
     });
 });
+
+// Confetti animation
+var skew = 1;
+var duration = 8 * 1000;
+var animationEnd = Date.now() + duration;
+
+var canvas = document.getElementById("banner");
+canvas.confetti = canvas.confetti || confetti.create(canvas, { resize: true });
+
+function randomInRange(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+(function frame() {
+    var timeLeft = animationEnd - Date.now();
+    var ticks = Math.max(200, 500 * (timeLeft / duration));
+    skew = Math.max(0.8, skew - 0.001);
+
+    canvas.confetti({
+        particleCount: 1,
+        startVelocity: 0,
+        ticks: ticks,
+        origin: {
+            x: Math.random(),
+            // since particles fall down, skew start toward the top
+            y: Math.random() * skew - 0.2,
+        },
+        colors: ["#ff005d"],
+        shapes: ["circle"],
+        gravity: randomInRange(0.4, 0.6),
+        scalar: randomInRange(0.4, 1),
+        drift: randomInRange(-0.4, 0.4),
+    });
+
+    if (timeLeft > 0) {
+        requestAnimationFrame(frame);
+    }
+})();

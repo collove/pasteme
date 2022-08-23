07ffdc017b3d732711e8f76bf05c6a658b965d31
Sadra Yahyapour <lnxpylnxpy@gmail.com>
2022-08-23 15:17:49 +0430

    static root issue fixed

--- pasteme/settings.py
+++ pasteme/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from contextlib import suppress
 from pathlib import Path
 
@@ -126,7 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = "static/"
+STATIC_ROOT = BASE_DIR / "static"
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = "media/"
